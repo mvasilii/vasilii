e08cc5eb4bf1c88cdb68f8509dda55cb2bc92947
Vasilii Mikhailov <vasilii.mikhailov@aiven.io>
2024-11-27 15:26:12 +0200

    prod

--- dev_update.sh
+++ dev_update.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 sudo systemctl restart chronyd
+op whoami > /dev/null 2>&1 || eval $(op signin)
 cd $AIVEN_CORE_PATH
 git fetch --all
 git checkout main
@@ -8,4 +9,4 @@ git pull origin main
 make build-dep-fedora
 make
 #aprod prod checkout-prod
-#avn-admin prod checkout-prod
+python3 -m aiven.admin --config op://private/aivendb_readonly/notesPlain prod checkout-prod
